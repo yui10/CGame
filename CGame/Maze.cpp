85902746ef584f53b6157ba35e6ff72cad873454
yui10 <79691301+yui10@users.noreply.github.com>
2023-05-24 11:36:23 +0900

    Update Maze.cpp
    定数を使用するように変更

--- CGame/Maze.cpp
+++ CGame/Maze.cpp
@@ -10,6 +10,12 @@
 
 #define MAP_SIZE 23
 
+#define WALL 0
+#define ROAD 1
+#define OUT_WALL 3
+#define START 5
+#define GOAL 6
+
 using namespace std;
 
 typedef struct {
@@ -42,7 +48,7 @@ int main() {
 		system("cls");
 		show();
 		printf("%d %d", cursorY, cursorX);
-		if (map[cursorY][cursorX] == 6) {
+		if (map[cursorY][cursorX] == GOAL) {
 			break;
 		}
 		backx = cursorX, backy = cursorY;
@@ -63,7 +69,7 @@ int main() {
 			return 0;
 		}
 
-		if (map[cursorY][cursorX] == 0 || map[cursorY][cursorX] == 3) {
+		if (map[cursorY][cursorX] == WALL || map[cursorY][cursorX] == OUT_WALL) {
 			cursorY = backy, cursorX = backx;
 		}
 
@@ -80,19 +86,19 @@ void show() {
 			}
 			else {
 				switch (map[i][j]) {
-				case 0:
+				case WALL:
 					printf("□");//壁
 					break;
-				case 1:
+				case ROAD:
 					printf("  ");//道
 					break;
-				case 3:
+				case OUT_WALL:
 					printf("■");//外壁
 					break;
-				case 5:
+				case START:
 					printf("ｓ");//スタート
 					break;
-				case 6:
+				case GOAL:
 					printf("ｇ");//ゴール
 					break;
 				}
@@ -106,18 +112,18 @@ void creatmap() {
 	int ay[4] = { -1,0,0,1 };
 	int ax[4] = { 0,-1,1,0 };
 	for (int i = 0; i < MAP_SIZE; i++) {
-		map[i][0] = 3;
-		map[i][MAP_SIZE - 1] = 3;
-		map[0][i] = 3;
-		map[MAP_SIZE - 1][i] = 3;
+		map[i][0] = OUT_WALL;
+		map[i][MAP_SIZE - 1] = OUT_WALL;
+		map[0][i] = OUT_WALL;
+		map[MAP_SIZE - 1][i] = OUT_WALL;
 	}
-	map[y][x] = 1;
+	map[y][x] = ROAD;
 	for (x = 1; x < MAP_SIZE - 1; x += 2) {
 		for (y = 1; y < MAP_SIZE - 1; y += 2) {
 
 			//for(y=2;y<mapsize-2;y+=2){
 			//	for (x = 2; x < mapsize-2; x+=2) {
-			if (map[y][x] == 1) {
+			if (map[y][x] == ROAD) {
 				int x1 = x, y1 = y;
 				int check = 0;
 				int count = 0;
@@ -128,9 +134,9 @@ void creatmap() {
 					int a = rand();
 					srand(a + check + x1 + y1 + count + y * x);
 					no = rand() % 4;
-					if (map[y1 + ay[no]][x1 + ax[no]] == 0 && map[y1 + ay[no] * 2][x1 + ax[no] * 2] == 0) {
-						map[y1 + ay[no]][x1 + ax[no]] = 1;//1マスとなり
-						map[y1 + ay[no] * 2][x1 + ax[no] * 2] = 1;//2マス先
+					if (map[y1 + ay[no]][x1 + ax[no]] == WALL && map[y1 + ay[no] * 2][x1 + ax[no] * 2] == WALL) {
+						map[y1 + ay[no]][x1 + ax[no]] = ROAD;//1マスとなり
+						map[y1 + ay[no] * 2][x1 + ax[no] * 2] = ROAD;//2マス先
 
 						y1 = y1 + ay[no] * 2;
 						x1 = x1 + ax[no] * 2;
@@ -184,20 +190,17 @@ void check() {
 			datacheck.qturn = xy.qturn;
 			datacheck.qX = xy.qX;
 			datacheck.qY = xy.qY;
-			if (checkmap[xy.qY + ay[i]][xy.qX + ax[i]] == 0 && map[xy.qY + ay[i]][xy.qX + ax[i]] == 1) {
+			if (checkmap[xy.qY + ay[i]][xy.qX + ax[i]] == 0 && map[xy.qY + ay[i]][xy.qX + ax[i]] == ROAD) {
 				que.push(queuedata{ datacheck.qX + ax[i],datacheck.qY + ay[i],datacheck.qturn + 1 });//追加
 			}
 			checkmap[datacheck.qY + ay[i]][datacheck.qX + ax[i]] = 1;
 		}
 	}
-	map[data.qY][data.qX] = 5;
+	map[data.qY][data.qX] = START;
 	//printf("\n");
 	cursorY = data.qY, cursorX = data.qX;
-	for (int i = 0; i < MAP_SIZE; i++) {
-		for (int j = 0; j < MAP_SIZE; j++) {
-			checkmap[i][j] = 0;
-		}
-	}
+	memset(checkmap, 0, sizeof(checkmap));
+
 	//printf("y%d x%d\n", data.qY, data.qX);
 	que.push(queuedata{ cursorX,cursorY, 0 });
 	data.qturn = -1;
@@ -218,12 +221,12 @@ void check() {
 			datacheck.qturn = xy.qturn;
 			datacheck.qX = xy.qX;
 			datacheck.qY = xy.qY;
-			if (checkmap[xy.qY + ay[i]][xy.qX + ax[i]] == 0 && map[xy.qY + ay[i]][xy.qX + ax[i]] == 1) {
+			if (checkmap[xy.qY + ay[i]][xy.qX + ax[i]] == 0 && map[xy.qY + ay[i]][xy.qX + ax[i]] == ROAD) {
 				que.push(queuedata{ datacheck.qX + ax[i],datacheck.qY + ay[i],datacheck.qturn + 1 });//追加
 			}
 			checkmap[datacheck.qY + ay[i]][datacheck.qX + ax[i]] = 1;
 		}
 	}
-	map[data.qY][data.qX] = 6;
+	map[data.qY][data.qX] = GOAL;
 
 }
